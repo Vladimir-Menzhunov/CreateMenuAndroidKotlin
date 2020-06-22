fafef81429925dc4e20a5d75605c065f286f168e
vladimir <vi.vova2000@gmail.com>
2020-06-22 14:48:23 +0300

    Create Menu

---
+++ app/src/main/java/com/vladimir/coursera/applicationcreatemenukotlin/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.vladimir.coursera.applicationcreatemenukotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings -> Toast.makeText(this, R.string.settings, Toast.LENGTH_SHORT).show()
+            R.id.search -> Toast.makeText(this, R.string.search, Toast.LENGTH_SHORT).show()
+            R.id.exit -> Toast.makeText(this, R.string.exit, Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
+
+
